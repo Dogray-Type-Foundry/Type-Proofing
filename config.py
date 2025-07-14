facde45aa1d4a901c1ebbf6888f79360a32963d2
Jose Sole <jose@dogray.xyz>
2025-07-14 20:36:36 +0100

    Minor comments

--- config.py
+++ config.py
@@ -79,6 +79,21 @@ DEFAULT_ON_FEATURES = {
     "dist",
     "rclt",
     "rvrn",
+    "curs",
+    "locl",
+}
+
+HIDDEN_FEATURES = {
+    "init",
+    "medi",
+    "med2",
+    "fina",
+    "fin2",
+    "fin3",
+    "isol",
+    "curs",
+    "aalt",
+    "rand",
 }
 
 # =============================================================================
